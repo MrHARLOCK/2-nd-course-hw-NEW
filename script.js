d3384e1f3d10951d49c739ec7f5875e0c37bf0dc
MrHARLOCK <rezni4enkog@yandex.ru>
2024-02-08 00:47:27 +0300

    Задание 3, 4, 5, 6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*Задание 1*/
-
+/*
 let password = 'пароль';
 
 let check = prompt('Введите пароль');
@@ -17,7 +17,7 @@ answer = (check === password) ? 'Пароль введен верно' : 'Пар
 alert(answer);*/
 
 /*Задание 2*/
-
+/*
 let c = 8;
 // c = 0;
 // c = 10;
@@ -29,3 +29,71 @@ if (c > 0 && c < 10) {
    console.log('Неверно');
 }
 
+/*Задание 3*/
+/*
+let d = 500;
+
+let e = 60;
+
+if (d > 100 || e > 100) {
+   console.log('Верно');
+} else {
+   console.log('Неверно');
+}
+
+/*Задание 4*/
+/*
+let a = '2';
+
+let b = '3';
+
+a = Number(a);
+
+b = Number(b);
+
+alert(a + b);
+
+/* Задание 5*/
+/*
+let monthNumber = Number(prompt('Введите номер месяца в году'));
+
+switch (monthNumber) {
+   case 1:
+      alert('Зима');
+      break;
+   case 2:
+      alert('Зима');
+      break;
+   case 3:
+      alert('Весна');
+      break;
+   case 4:
+      alert('Весна');
+      break;
+   case 5:
+      alert('Весна');
+      break;
+   case 6:
+      alert('Лето');
+      break;
+   case 7:
+      alert('Лето');
+      break;
+   case 8:
+      alert('Лето');
+      break;
+   case 9:
+      alert('Осень');
+      break;
+   case 10:
+      alert('Осень');
+      break;
+   case 11:
+      alert('Осень');
+      break;
+   case 12:
+      alert('Зима');
+      break;
+   default: alert('Не верный номер!!!')
+      break;
+}
