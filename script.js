c78358c008a13df8cf235c2fc36d14121f315b19
MrHARLOCK <rezni4enkog@yandex.ru>
2024-02-07 23:39:52 +0300

    Задание 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*Задание 1*/
+
 let password = 'пароль';
 
 let check = prompt('Введите пароль');
@@ -14,3 +15,17 @@ let answer;
 answer = (check === password) ? 'Пароль введен верно' : 'Пароль введен неправильно';
 
 alert(answer);*/
+
+/*Задание 2*/
+
+let c = 8;
+// c = 0;
+// c = 10;
+// c = -2;
+// c = 2;
+if (c > 0 && c < 10) {
+   console.log('Верно');
+} else {
+   console.log('Неверно');
+}
+
