22618ed855b38c3708ac3a9c95c242a2ac33b329
MrHARLOCK <rezni4enkog@yandex.ru>
2024-02-14 12:28:59 +0300

    Задания 5-7

--- script.js
+++ script.js
@@ -63,4 +63,65 @@ function howOld() {
 howOld();
 
 /*Задание 5*/
+/*
+function checkingNumbers(numOne, numTwo) {
+   if ((isNaN(numOne)) || (isNaN(numTwo))) {
+      return ('Одно или оба значения не являются числом');
+   } else {
+      return (numOne * numTwo);
+   }
+}
 
+console.log(checkingNumbers(2, 'ДВА'));
+console.log(checkingNumbers(2, 2));
+console.log(checkingNumbers('2', 2));
+console.log(checkingNumbers('2', '2'));
+
+/*Задание 6*/
+/*
+function countingNumbers() {
+   const userAnswer = Number(prompt('Введите число.'));
+
+   if ((isNaN(userAnswer))) {
+      return ('Переданный параметр не является числом');
+   } else if (!userAnswer) {
+      alert('НЕ ЗАДУРЯЙСЯ ;)');
+   } else {
+      return (`${userAnswer} в кубе равняется ${userAnswer ** 3}`);
+   }
+}
+
+console.log(countingNumbers());
+
+/*Задание 7*/
+/*
+function getRectangleArea() {
+   return (this.radius ** 2) * 3.14;
+}
+
+function getRectanglePerimeter() {
+   return (this.radius * 2) * 3.14;
+}
+
+const circle1 = {
+   radius: 5,
+
+   getArea: getRectangleArea,
+   getPerimeter: getRectanglePerimeter,
+}
+
+const circle2 = {
+   radius: 8,
+
+   getArea: getRectangleArea,
+   getPerimeter: getRectanglePerimeter,
+}
+
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
+
+/*Задание 8*/
+
+/* Находится в файле index.js*/
