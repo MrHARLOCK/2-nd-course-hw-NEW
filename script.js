a53fb62933dc539190d5b76c91eb873c1f8f457e
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-13 19:19:48 +0300

    /*Задание 1*/

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
 /*Задание 1*/
-/*
-let hello = 1;
 
-while (hello <= 2) {
-   alert('Привет');
-   hello++;
-}
-
-/*Задание 2*/
-/*
-let num = 1;
-
-while (num <= 5) {
-   console.log(num);
-   num++;
-}
-
-/*Задание 3*/
-/*
-let numSeven = 7;
-
-while (numSeven <= 22) {
-   console.log(numSeven);
-   numSeven++;
-}
-
-/*Задание 4*/
-/*
-
-}
-
-/*Задание 5*/
+let string = 'js';
+console.log(string.toUpperCase());
 
