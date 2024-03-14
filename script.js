0af4cc20114383510344ee6642840796e3dd6d6b
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-15 00:38:01 +0300

    /*Задание 2*/  стрелочная функция

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// /*Задание 1*/
+/*Задание 1*/
 
 // let text = 'js';
 // console.log(text.toUpperCase());
 
 /*Задание 2*/
 
-function searchStart(arr, str) {
+const searchStart = (arr, str) => {
   return arr.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
 }
 
 console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
 console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
-console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));   
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+
+/*Задание 3*/
+
