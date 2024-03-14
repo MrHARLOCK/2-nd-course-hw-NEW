6af174b82446abd1809131c875ca45dab8fc5425
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-15 01:15:00 +0300

    /*Задание 3*/

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-/*Задание 1*/
+// /*Задание 1*/
 
 // let text = 'js';
 // console.log(text.toUpperCase());
 
-/*Задание 2*/
+// /*Задание 2*/
 
-const searchStart = (arr, str) => {
-  return arr.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
-}
+// const searchStart = (arr, str) => {
+//   return arr.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
+// }
 
-console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
-console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
-console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
+// console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+// console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+// console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
 
-/*Задание 3*/
+// /*Задание 3*/
 
+// let num = 32.58884
+
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+
+// /*Задание 4*/
