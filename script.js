997fd2582d7e96fc215f26f3a5342a0fe3ee131b
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-15 00:25:51 +0300

    /*Задание 2*/

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-/*Задание 1*/
+// /*Задание 1*/
 
-let string = 'js';
-console.log(string.toUpperCase());
+// let text = 'js';
+// console.log(text.toUpperCase());
 
+/*Задание 2*/
+
+function searchStart(arr, str) {
+  return arr.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
+}
+
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));   
