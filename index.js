afeb7bca41925a123ec32bd67f45028c0a8238fb
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-20 12:23:22 +0300

    2

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ function gameSite() {
    }
 }
 
+function seasonsBtn() {
+   let words = ['яблоко', 'груша', 'дыня', 'виноград', 'персик', 'апельсин', 'мандарин'];
+   words = words.sort(() => Math.random() - 0.5);
+
+   alert(words);
+
+   let wordsfirst = prompt(`Назовите первое слово`);
+   wordsfirst = wordsfirst.toLowerCase();
+   let wordsLast = prompt(`Назовите последнее слово`);
+   wordsLast = wordsLast.toLowerCase();
+   
+   if (wordsfirst === words[0] && wordsLast === words[words.length - 1]) {
+      alert`Это правильный ответ!`;
+   } else if (wordsfirst === words[0] || wordsLast === words[words.length - 1]) {
+      alert`Вы были близки к победе!`;
+   }
+   else {
+      alert`Вы не угадали!`;
+   }
+}
