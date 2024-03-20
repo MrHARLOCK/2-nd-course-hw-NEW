d1d25bbe7731d7f030afb9d5a0fa90e899688f27
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-20 12:22:45 +0300

    1

--- script.js
+++ script.js
@@ -1,31 +1,115 @@
 /*Задание 1*/
 
-
-// const arr = [1, 5, 4, 10, 0, 3];
-
-// for (let item = 0; item < arr.length; item++) {
-//    console.log(arr[item]);
-//    if (arr[item] === 10) break;
-// }
+let text = 'js';
+console.log(text.toUpperCase());
 
 /*Задание 2*/
 
+const searchStart = (arr, str) => {
+   return arr.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
+}
 
-// console.log(arr.indexOf(4));
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
 
 /*Задание 3*/
 
-// let multArr = [];
+let num = 32.58884
 
-// for (let i = 0; i < 3; i++) {
-//    multArr[i] = [];
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
 
-//    for (let j = 0; j < 3; j++) {
-//       multArr[i].push(1);
-//    }
-// }
+/*Задание 4*/
 
-// console.log(multArr);
+const arrNum = [52, 53, 49, 77, 21, 32];
+
+function getMaxOfArray(arrNum) {
+   return Math.max.apply(null, arrNum);
+}
+function getMinOfArray(arrNum) {
+   return Math.min.apply(null, arrNum);
+}
+
+console.log(getMaxOfArray(arrNum));
+console.log(getMinOfArray(arrNum));
 
 /*Задание 5*/
 
+function getRandomInt() {
+   return Math.floor(Math.random() * 10);
+}
+
+console.log(getRandomInt());
+
+/*Задание 6*/
+
+function numRandom(num) {
+   const arr = [];
+   const n = Math.floor(num / 2);
+
+   for (let i = 0; i < n; i++) {
+      arr.push(Math.floor(Math.random() * num + 1));
+   }
+
+   return arr;
+}
+
+console.log(numRandom(15));
+
+/*Задание 7*/
+
+function getRandomIntInclusive(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(getRandomIntInclusive(5, 9));
+
+function getRandomArbitrary(min, max) {
+   return Math.random() * (max - min) + min;
+}
+
+console.log(getRandomArbitrary(5, 9));
+
+/*Задание 8*/
+
+let currentDate = new Date();
+
+console.log(currentDate);
+
+/*Задание 9*/
+
+let myDate = new Date();
+
+console.log(+myDate);
+
+let daysSeventyThree = 73 * 24 * 60 * 60 * 1000;
+let searchDate = +myDate + daysSeventyThree;
+let inSeventyThreeDays = new Date(searchDate);
+
+console.log(`Через 73 дня будет ${inSeventyThreeDays}`);
+console.log(`Сегодня ${myDate}`);
+
+/*Задание 10*/
+
+function ruDate() {
+   const currentDate = new Date();
+   const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+   };
+   const optionsDay = {
+      weekday: 'long'
+   };
+   const formattedDate = currentDate.toLocaleDateString('ru-RU', options);
+   const formattedDay = currentDate.toLocaleDateString('ru-RU', optionsDay);
+
+   return console.log(`Дата: ${formattedDate} — это ${formattedDay}\nВремя: ${currentDate.toLocaleTimeString('ru-RU')}`);
+}
+ruDate()
+
+/*Задание 11. Работа с макетом*/
