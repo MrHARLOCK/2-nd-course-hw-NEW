0ad10d1f6bba06573f7eeda457c691e146a8665c
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-22 00:28:15 +0300

    задание 2

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ console.log(people.sort((a, b) => {
 }));
 
 /*Задание 2*/
+
+function isPositive(num) {
+   return num > 0;
+}
+
+function isMale(men) {
+   return men.gender === 'male';
+}
+
+function filter(arr, ruleFunction) {
+   const output = [];
+   for (let item of arr) {
+      if (ruleFunction(item)) {
+         output.push(item);
+      }
+   }
+   return output;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const folk = [
+   { name: 'Глеб', gender: 'male' },
+   { name: 'Анна', gender: 'female' },
+   { name: 'Олег', gender: 'male' },
+   { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(folk, isMale));
