9b5095feef8a2bf6360d5c7b8273cc3aaee42d2e
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-21 09:39:55 +0300

    1

--- script.js
+++ script.js
@@ -1,34 +1,115 @@
-// const fibonacci = [1, 1, 2, 3, 5, 8];
-// const multipleByTwoFib = fibonacci.map((num) => num * 2);
-// const plusTenFib = fibonacci.map((num) => num + 10);
-// const onlyBigFib = fibonacci.filter((num) => num > 3);
-// console.log(multipleByTwoFib);
-// console.log(plusTenFib);
-// console.log(onlyBigFib);
+/*Задание 1*/
 
-// function callbackWithArrayLength(arr, callback) {
-//    console.log(arr);
-//    callback(arr.length);
-// }
-// callbackWithArrayLength([1], (length) => {
-//    console.log(length);
-// });
+let text = 'js';
+console.log(text.toUpperCase());
 
-// callbackWithArrayLength([1, 1], (len) => {
-//    console.log(len);
-// });
-// callbackWithArrayLength([1, 1, 1, 1, 1], (l) => {
-//    console.log(l);
-// });
+/*Задание 2*/
 
-// function log(arrItem) {
-//    console.log("Элемент массива:", arrItem);
-// }
+const searchStart = (arr, str) => {
+   return arr.filter(el => el.toLowerCase().startsWith(str.toLowerCase()));
+}
 
-// function each(arr, cb) {
-//    for (let i = 0; i < arr.length; i++) {
-//       cb(arr[i]);
-//    }
-// }
+console.log(searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'));
+console.log(searchStart(['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'));
+console.log(searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'));
 
-// each(["Глеб", "Олег", "Татьяна", "Платон"], log);
+/*Задание 3*/
+
+let num = 32.58884
+
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+console.log(Math.round(num));
+
+/*Задание 4*/
+
+const arrNum = [52, 53, 49, 77, 21, 32];
+
+function getMaxOfArray(arrNum) {
+   return Math.max.apply(null, arrNum);
+}
+function getMinOfArray(arrNum) {
+   return Math.min.apply(null, arrNum);
+}
+
+console.log(getMaxOfArray(arrNum));
+console.log(getMinOfArray(arrNum));
+
+/*Задание 5*/
+
+function getRandomInt() {
+   return Math.floor(Math.random() * 10);
+}
+
+console.log(getRandomInt());
+
+/*Задание 6*/
+
+function numRandom(num) {
+   const arr = [];
+   const n = Math.floor(num / 2);
+
+   for (let i = 0; i < n; i++) {
+      arr.push(Math.floor(Math.random() * num + 1));
+   }
+
+   return arr;
+}
+
+console.log(numRandom(15));
+
+/*Задание 7*/
+
+function getRandomIntInclusive(min, max) {
+   min = Math.ceil(min);
+   max = Math.floor(max);
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+console.log(getRandomIntInclusive(5, 9));
+
+function getRandomArbitrary(min, max) {
+   return Math.random() * (max - min) + min;
+}
+
+console.log(getRandomArbitrary(5, 9));
+
+/*Задание 8*/
+
+let currentDate = new Date();
+
+console.log(currentDate);
+
+/*Задание 9*/
+
+let myDate = new Date();
+
+console.log(+myDate);
+
+let daysSeventyThree = 73 * 24 * 60 * 60 * 1000;
+let searchDate = +myDate + daysSeventyThree;
+let inSeventyThreeDays = new Date(searchDate);
+
+console.log(`Через 73 дня будет ${inSeventyThreeDays}`);
+console.log(`Сегодня ${myDate}`);
+
+/*Задание 10*/
+
+function ruDate() {
+   const currentDate = new Date();
+   const options = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+   };
+   const optionsDay = {
+      weekday: 'long'
+   };
+   const formattedDate = currentDate.toLocaleDateString('ru-RU', options);
+   const formattedDay = currentDate.toLocaleDateString('ru-RU', optionsDay);
+
+   return console.log(`Дата: ${formattedDate} — это ${formattedDay}\nВремя: ${currentDate.toLocaleTimeString('ru-RU')}`);
+}
+ruDate()
+
+/*Задание 11. Работа с макетом*/
