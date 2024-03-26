215a947ca164650a8aa3df58db86a13761acf8dd
MrHARLOCK <rezni4enkog@yandex.ru>
2024-03-26 14:30:58 +0300

    Задание 5

--- script.js
+++ script.js
@@ -1,72 +1,90 @@
-/*Задание 1*/
+// /*Задание 1*/
 
-const people = [
-   { name: 'Глеб', age: 29 },
-   { name: 'Анна', age: 17 },
-   { name: 'Олег', age: 7 },
-   { name: 'Оксана', age: 47 }
-];
+// const people = [
+//    { name: 'Глеб', age: 29 },
+//    { name: 'Анна', age: 17 },
+//    { name: 'Олег', age: 7 },
+//    { name: 'Оксана', age: 47 }
+// ];
 
-console.log(people.sort((a, b) => {
-   if (a.age > b.age)
-      return 1;
-   if (a.age < b.age) {
-      return -1;
-   }
-   return 0;
-}));
+// console.log(people.sort((a, b) => {
+//    if (a.age > b.age)
+//       return 1;
+//    if (a.age < b.age) {
+//       return -1;
+//    }
+//    return 0;
+// }));
 
-/*Задание 2*/
+// /*Задание 2*/
 
-function isPositive(num) {
-   return num > 0;
-}
+// function isPositive(num) {
+//    return num > 0;
+// }
 
-function isMale(men) {
-   return men.gender === 'male';
-}
+// function isMale(men) {
+//    return men.gender === 'male';
+// }
 
-function filter(arr, ruleFunction) {
-   const output = [];
-   for (let item of arr) {
-      if (ruleFunction(item)) {
-         output.push(item);
+// function filter(arr, ruleFunction) {
+//    const output = [];
+//    for (let item of arr) {
+//       if (ruleFunction(item)) {
+//          output.push(item);
+//       }
+//    }
+//    return output;
+// }
+
+// console.log(filter([3, -4, 1, 9], isPositive));
+
+// const folk = [
+//    { name: 'Глеб', gender: 'male' },
+//    { name: 'Анна', gender: 'female' },
+//    { name: 'Олег', gender: 'male' },
+//    { name: 'Оксана', gender: 'female' }
+// ];
+
+// console.log(filter(folk, isMale));
+
+// /*Задание 3*/
+
+// let timeCounter = 0;
+
+// const timeInterval = setInterval(() => {
+//    console.log(new Date());
+//    timeCounter += 3;
+//    if (timeCounter >= 30) {
+//       clearInterval(timeInterval);
+
+//       console.log('30 секунд прошло');
+//    }
+// }, 3000);
+
+// /*Задание 4*/
+
+// function delayForSecond(callback) {
+//    setTimeout(callback, 1000);
+// }
+
+// delayForSecond(function () {
+//    console.log('Привет, Глеб!');
+// })
+
+/*Задание 5*/
+
+
+function delayForSecond(cb) {
+   setTimeout(() => {
+      console.log('Прошла одна секунда');
+      if (cb) {
+         cb();
       }
-   }
-   return output;
+   }, 1000);
 }
 
-console.log(filter([3, -4, 1, 9], isPositive));
-
-const folk = [
-   { name: 'Глеб', gender: 'male' },
-   { name: 'Анна', gender: 'female' },
-   { name: 'Олег', gender: 'male' },
-   { name: 'Оксана', gender: 'female' }
-];
-
-console.log(filter(folk, isMale));
-
-/*Задание 3*/
-
-let timeCounter = 0;
-
-const timeInterval = setInterval(() => {
-   console.log(new Date());
-   timeCounter += 3;
-   if (timeCounter >= 30) {
-      clearInterval(timeInterval);
-
-      console.log('30 секунд прошло');
-   }
-}, 3000);
-
-/*Задание 4*/
-
-function delayForSecond(callback) {
-   setTimeout(callback, 1000);
+function sayHi(name) {
+   console.log(`Привет, ${name}!`);
 }
 
-delayForSecond(function () {
-   console.log('Привет, Глеб!');
-})
+delayForSecond(() => sayHi('Глеб'))
